f46407b3899328bd4aeece689fccf9a0311fea92
Josh Long <josh@joshlong.com>
2024-04-13 14:41:42 -0700

    up

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ GH_ORG=git@github.com:bootiful-media-mogul
 CLONE_DIR=$HOME/code/mogul
 mkdir -p $CLONE_DIR
 
-for i in mogul-audio-processor authorization mogul-gateway mogul-client mogul-service workspace pipeline; do
+for i in mogul-audio-processor authorization-service mogul-gateway mogul-client mogul-service workspace pipeline; do
     FULL_CLONE_DIR=$CLONE_DIR/$i
     if [ -d "$FULL_CLONE_DIR" ]; then
       echo the directory "$FULL_CLONE_DIR" already exists

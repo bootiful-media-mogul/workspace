c419f694e16463fea423393649117d0acc2c3f49
Josh Long <josh@joshlong.com>
2024-04-13 14:29:10 -0700

    up

--- init.sh
+++ init.sh
@@ -8,12 +8,12 @@ for i in mogul-audio-processor authorization mogul-gateway mogul-client mogul-se
     FULL_CLONE_DIR=$CLONE_DIR/$i
     if [ -d "$FULL_CLONE_DIR" ]; then
       echo "$FULL_CLONE_DIR does exist."
-      git clone  $GH_ORG/${i}.git $FULL_CLONE_DIR
+      git clone  $GH_ORG/${i}.git "$FULL_CLONE_DIR"
     else
-      echo the directory $FULL_CLONE_DIR already exists
-      cd $FULL_CLONE_DIR
+      echo the directory "$FULL_CLONE_DIR" already exists
+      cd "$FULL_CLONE_DIR"
       git pull || echo "couldn't git pull the latest revisions in $FULL_CLONE_DIR "
     fi
 done
 
-cp
+cp new-envrc $CLONE_DIR/.envrc

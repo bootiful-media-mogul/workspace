7d858cd6375e3f876c7693c959ac8eb1f2f09238
Josh Long <josh@joshlong.com>
2024-04-13 14:34:18 -0700

    gi

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+HERE="$(cd  `dirname $0 ` && pwd )"
 GH_ORG=git@github.com:bootiful-media-mogul
 CLONE_DIR=$HOME/code/mogul
 mkdir -p $CLONE_DIR
@@ -7,14 +7,15 @@ mkdir -p $CLONE_DIR
 for i in mogul-audio-processor authorization mogul-gateway mogul-client mogul-service workspace pipeline; do
     FULL_CLONE_DIR=$CLONE_DIR/$i
     if [ -d "$FULL_CLONE_DIR" ]; then
-      echo "$FULL_CLONE_DIR does exist."
-      git clone  $GH_ORG/${i}.git "$FULL_CLONE_DIR"
-    else
       echo the directory "$FULL_CLONE_DIR" already exists
       cd "$FULL_CLONE_DIR"
       git pull || echo "couldn't git pull the latest revisions in $FULL_CLONE_DIR "
+    else
+      echo "$FULL_CLONE_DIR does exist."
+      git clone  $GH_ORG/${i}.git "$FULL_CLONE_DIR"
     fi
 done
 
-cp new-envrc $CLONE_DIR/.envrc
-cp compose.yml $CLONE_DIR/compose.yml
+
+cp "$HERE"/new-envrc "$CLONE_DIR"/.envrc
+cp "$HERE"/compose.yaml "$CLONE_DIR"/compose.yaml

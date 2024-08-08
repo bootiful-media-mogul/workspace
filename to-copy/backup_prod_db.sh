a789c3cd60797c8d32648b702ab759f406495c6e
Josh Long <josh@joshlong.com>
2024-08-08 13:54:52 -0700

    up

--- to-copy/backup_prod_db.sh
+++ to-copy/backup_prod_db.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-export BW_SESSION=$(bw unlock --raw)
+export BW_SESSION=${BW_SESSION:-$(bw unlock --raw)}
+echo "got the BW_SESSION: ${BW_SESSION}"
 bw get item crunchydata-mogul-db-production   | jq -r '.notes'  | /bin/bash 

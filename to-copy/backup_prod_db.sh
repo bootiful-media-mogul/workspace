4f0744ae6df757fc2e44735b5b2ccd2b46afc205
Josh Long <josh@joshlong.com>
2024-08-24 16:10:14 -0700

    up

--- to-copy/backup_prod_db.sh
+++ to-copy/backup_prod_db.sh
@@ -2,5 +2,5 @@
 export BW_SESSION=${BW_SESSION:-$(bw unlock --raw)}
 echo "got the BW_SESSION: ${BW_SESSION}"
 export SQL_FILE=${SQL_FILE:-$HOME/Desktop/output.sql}
-bw get item crunchydata-mogul-db-production | jq -r '.notes' | /bin/bash
+bw get item mogul-gke-postgresql-db--production | jq -r '.notes' | /bin/bash
 ls -la $SQL_FILE || echo "there's no .SQL file backup to be found."

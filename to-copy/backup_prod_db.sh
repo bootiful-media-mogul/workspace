32345c831f0a515ee868a7f2e0f97599881bfd9a
Josh Long <josh@joshlong.com>
2024-08-30 00:48:42 -0700

    polish

--- to-copy/backup_prod_db.sh
+++ to-copy/backup_prod_db.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 export BW_SESSION=${BW_SESSION:-$(bw unlock --raw)}
-echo "got the BW_SESSION: ${BW_SESSION}"
-export SQL_FILE=${SQL_FILE:-$HOME/Desktop/output.sql}
-bw get item mogul-gke-postgresql-db--production | jq -r '.notes' | /bin/bash
-ls -la $SQL_FILE || echo "there's no .SQL file backup to be found."
+
+SQL_FILE=${SQL_FILE:-$HOME/Desktop/pg_dump.sql}
+GKE_SQL_CREDS=$( bw get item mogul-gke-postgresql-db--production )
+DB_PW=$( echo $GKE_SQL_CREDS |    jq '.. | objects | select(.name == "password" ) '  | jq -r .value )
+DB_HOST=$( echo $GKE_SQL_CREDS |    jq '.. | objects | select(.name == "hostname" ) '  | jq -r .value )
+DB_DB=$( echo $GKE_SQL_CREDS |    jq '.. | objects | select(.name == "database" ) '  | jq -r .value )
+DB_USER=$( echo $GKE_SQL_CREDS |    jq '.. | objects | select(.name == "username" ) '  | jq -r .value )
+
+#echo $DB_PW
+#echo $DB_HOST
+#echo $DB_USER
+#echo $DB_DB
+
+PGPASSWORD=$DB_PW pg_dump  --no-owner --no-privileges --if-exists -c --create \
+  -U $DB_USER -h $DB_HOST $DB_DB >  $SQL_FILE

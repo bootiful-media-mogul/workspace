74643dba53292f7735718d692d794ddedfe61207
Josh Long <josh@joshlong.com>
2024-10-15 11:19:47 +0200

    up

--- to-copy/setup_local_dev_from_prod_db.sh
+++ to-copy/setup_local_dev_from_prod_db.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 export SQL_FILE=$HOME/Desktop/for-local.sql
-/bin/bash ./backup_prod_db.sh
+ls -la $SQL_FILE || /bin/bash ./backup_prod_db.sh
 cat $SQL_FILE | PGPASSWORD=mogul psql -U mogul -h localhost mogul

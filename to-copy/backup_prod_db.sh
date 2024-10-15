628f610be3aa555d600730671e9294595be17e4f
Josh Long <josh@joshlong.com>
2024-10-15 11:23:58 +0200

    backup

--- to-copy/backup_prod_db.sh
+++ to-copy/backup_prod_db.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export BW_SESSION=${BW_SESSION:-$(bw unlock --raw)}
-
+echo "SQL_FILE is ${SQL_FILE}"
 SQL_FILE=${SQL_FILE:-$HOME/Desktop/pg_dump.sql}
 SQL_CREDS=$( bw get item mogul-crunchydata-db-production )
 DB_PW=$( echo $SQL_CREDS |    jq '.. | objects | select(.name == "password" ) '  | jq -r .value )
@@ -8,10 +8,10 @@ DB_HOST=$( echo $SQL_CREDS |    jq '.. | objects | select(.name == "hostname" ) 
 DB_DB=$( echo $SQL_CREDS |    jq '.. | objects | select(.name == "database" ) '  | jq -r .value )
 DB_USER=$( echo $SQL_CREDS |    jq '.. | objects | select(.name == "username" ) '  | jq -r .value )
 
-#echo $DB_PW
-#echo $DB_HOST
-#echo $DB_USER
-#echo $DB_DB
+echo $DB_PW
+echo $DB_HOST
+echo $DB_USER
+echo $DB_DB
 
 PGPASSWORD=$DB_PW pg_dump  --no-owner --no-privileges --if-exists -c --create \
   -U $DB_USER -h $DB_HOST $DB_DB >  $SQL_FILE

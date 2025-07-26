9568d441cb1d2677125d86f41250f40cdb720e35
Josh Long <54473+joshlong@users.noreply.github.com>
2025-07-25 17:05:07 -0700

    prod

--- to-copy/prod_db.sh
+++ to-copy/prod_db.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 export BW_SESSION=${BW_SESSION:-$(bw unlock --raw)}
 
-DB_CREDS=`bw get item mogul-gke-postgresql-db--production`
+DB_CREDS=`bw get item mogul-crunchydata-db-production `
 
 USERNAME=$( echo $DB_CREDS |  jq -r '.fields[] | select(.name == "username") | .value' )
 PASSWORD=$( echo $DB_CREDS |  jq -r '.fields[] | select(.name == "password") | .value' )
-HOSTNAME=$( echo $DB_CREDS |  jq -r '.fields[] | select(.name == "hostname") | .value' )
+HOSTNAME=$( echo $DB_CREDS |  jq -r '.fields[] | select(.name == "host") | .value' )
 DB=$( echo $DB_CREDS |  jq -r '.fields[] | select(.name == "database") | .value' )
 
-# echo $USERNAME
-# echo $DB 
-# echo $PASSWORD
-# echo $HOSTNAME
+echo $USERNAME
+echo $DB
+echo $PASSWORD
+echo $HOSTNAME
 
-PGPASSWORD=$PASSWORD psql "sslmode=disable dbname=$DB user=$USERNAME hostaddr=${HOSTNAME}"
+# PGPASSWORD=$PASSWORD psql "sslmode=disable dbname=$DB user=$USERNAME hostaddr=postgres://postgres:${HOSTNAME}"
+psql postgres://${USERNAME}:${PASSWORD}@${HOSTNAME}:5432/${DB}

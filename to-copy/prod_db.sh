1f62a37aea41e5b56c4e21a1482cf7cda6edd4d8
Josh Long <joshua.long@broadcom.com>
2026-09-16 00:44:53 -0700

    produ

--- to-copy/prod_db.sh
+++ to-copy/prod_db.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 export BW_SESSION=${BW_SESSION:-$(bw unlock --raw)}
 
-DB_CREDS=`bw get item mogul-crunchydata-db-production `
-
+# DB_CREDS=`bw get item mogul-crunchydata-db-production `
+DB_CREDS=$( bw get item 15ecc361-ac4b-42fb-98a1-b20701358338 )
 USERNAME=$( echo $DB_CREDS |  jq -r '.fields[] | select(.name == "username") | .value' )
 PASSWORD=$( echo $DB_CREDS |  jq -r '.fields[] | select(.name == "password") | .value' )
 HOSTNAME=$( echo $DB_CREDS |  jq -r '.fields[] | select(.name == "host") | .value' )

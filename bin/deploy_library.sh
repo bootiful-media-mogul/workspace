ecb66702b524753d3efd308f0dca14c5dc843a39
Josh Long <joshua.long@broadcom.com>
2026-09-21 00:48:52 -0700

    ws

--- bin/deploy_library.sh
+++ bin/deploy_library.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
 SETTINGS="${MOGUL_HOME}/workspace/bin/settings.xml"
-
-echo "deploying $(pwd) as ${GH_USER} using ${SETTINGS}"
 ./mvnw -s "$SETTINGS" deploy "$@"

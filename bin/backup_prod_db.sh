227fba5acd5b4bbba484088761dadeade8d24b38
Josh Long <joshua.long@broadcom.com>
2026-09-21 00:43:10 -0700

    deploy

--- bin/backup_prod_db.sh
+++ bin/backup_prod_db.sh
@@ -3,9 +3,9 @@ export BW_SESSION=${BW_SESSION:-$(bw unlock --raw)}
 SQL_FILE=${SQL_FILE:-$HOME/Desktop/pg_dump.sql}
 # SQL_CREDS=$( bw get item mogul-crunchydata-db-production )
 SQL_CREDS=$( bw get item 15ecc361-ac4b-42fb-98a1-b20701358338 )
-DB_PW=$( echo $SQL_CREDS |    jq '.. | objects | select(.name == "password" ) '  | jq -r .value )
-DB_HOST=$( echo $SQL_CREDS |    jq '.. | objects | select(.name == "host" ) '  | jq -r .value )
-DB_DB=$( echo $SQL_CREDS |    jq '.. | objects | select(.name == "database" ) '  | jq -r .value )
-DB_USER=$( echo $SQL_CREDS |    jq '.. | objects | select(.name == "username" ) '  | jq -r .value )
+DB_PW=$( echo $SQL_CREDS | jq '.. | objects | select(.name == "password" ) '  | jq -r .value )
+DB_HOST=$( echo $SQL_CREDS | jq '.. | objects | select(.name == "host" ) '  | jq -r .value )
+DB_DB=$( echo $SQL_CREDS | jq '.. | objects | select(.name == "database" ) '  | jq -r .value )
+DB_USER=$( echo $SQL_CREDS | jq '.. | objects | select(.name == "username" ) '  | jq -r .value )
 PGPASSWORD=$DB_PW pg_dump  --no-owner --no-privileges --if-exists -c --create \
   -U $DB_USER -h $DB_HOST $DB_DB >  $SQL_FILE

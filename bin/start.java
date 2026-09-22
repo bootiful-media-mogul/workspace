9a8f64adff73d874d5f0285a5af828a115316837
Josh Long <54473+joshlong@users.noreply.github.com>
2026-09-22 10:50:58 -0700

    starts smarter

--- bin/start.java
+++ bin/start.java
@@ -16,7 +16,7 @@ void main(String[] args) throws Exception {
 
     try (var ex = Executors.newCachedThreadPool()) {
 
-        var repositories = ("clip gateway client api workspace pipeline media-processor")
+        var repositories = ("clip gateway client api workspace pipeline media-processor github-actions-setup-jvm-build-env github-actions-setup-container-build-env")
                 .split(" ");
 
         var waiting = new HashSet<Future<?>>();
